c38483463fd3e9ca0c8250a6ba611d07f26c0632
Ankit Mishra <ankit.kumar.mishra2006@gmail.com>
2025-05-24 00:51:08 +0530

    backend - socketio Done

--- backend/src/routes/message.routes.js
+++ backend/src/routes/message.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/users");
-router.get("/:id");
-router.post("/send/:id");
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
